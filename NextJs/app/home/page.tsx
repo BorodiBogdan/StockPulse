30563e57bdbb775cfef43569f816232c4390d53b
BorodiBogdan <bogdanborodiv@gmail.com>
2024-08-22 20:54:03 +0300

    hardcoded api

--- NextJs/app/home/page.tsx
+++ NextJs/app/home/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
     const stocks: any = [];
 
     // Use fetch instead of axios
-    const response = await fetch(`http://localhost:3000/api/allstocks`, {
+    const response = await fetch(`https://activity-finder-roan.vercel.app/api/allstocks`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
